94c99762e79efd85bb0f75f8f24ded343bc6958d
Beatriz Braga <beatrizsagrado9@gmail.com>
2026-03-02 13:23:18 -0300

    fix: arrumei uma palavra errada

--- Atividade_Dia02-03/exercicio3.js
+++ Atividade_Dia02-03/exercicio3.js
@@ -18,7 +18,7 @@ let descontos;
 let salaroLiquido;
 
 //entrada
-salarioBruto = leia.questionFloat("Digte o salario bruto: ");
+salarioBruto = leia.questionFloat("Digite o salario bruto: ");
 adicionalNoturno = leia.questionFloat("Digite o adicional noturno: ");
 horasExtras = leia.questionFloat("Digite as horas extras: ");
 descontos = leia.questionFloat("Digite os descontos: ");
